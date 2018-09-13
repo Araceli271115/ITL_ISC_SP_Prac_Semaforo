7c010821e4389edf45a3a4c0787c0cdd1dfde5ec
araceli <marlenixgc07@gmail.com>
2018-09-12 20:26:43 -0500

    Implementacion de boton para cambiar el prendido de leds

--- SP_Semaforo_AMGC.ino
+++ SP_Semaforo_AMGC.ino
@@ -2,28 +2,29 @@
 #define led_verde 13                //definimos las variables y los pines en los que se encuentran
 #define led_amarillo 11
 #define led_rojo 9
-
-boolean valor_led_verde =HIGH;      //inicializamos el led_vere como encendido
-boolean valor_led_amarillo =LOW;    //inicializamos el led_amarillo como apagado
-boolean valor_led_rojo =LOW;        //inicializamos el led rojo como apagado
+#define boton_1 7
 
 void setup() {
   Serial.begin(9600);             //inicializar el puerto serie
   pinMode (led_verde, OUTPUT);    //pone al pin para el led verde en modo salida
   pinMode (led_amarillo, OUTPUT); //pone al pin para el led amarillo en modo salida
   pinMode (led_rojo, OUTPUT);     //pone al pin para el led rojo en modo salida
-  
+    pinMode (boton_1,INPUT);        //pone al pin para el boton en modo entrada
 }
 void loop() {
   digitalWrite(led_verde,HIGH);   //se aprende led Verde
-  delay(1000);                   //se da un tiempo de espera de transicion
-  digitalWrite(led_verde,LOW);    //se apaga led verde
-  digitalWrite(led_amarillo,HIGH);      //se prende el led amarillo 
-  delay(2000);                          //se da un tiempo en el que el led amarillo estará encendido
-  digitalWrite(led_amarillo,LOW);     //se apaga el led amarillo
-  delay(2000);                    //tiempo de espera de transicion entre led amarillo y rojo
-  digitalWrite(led_rojo,HIGH);              //se enciende el led rojo 
-  delay(4000);                                        //tiempo en el que el led rojo estara encendido
-  digitalWrite(led_rojo,LOW);             //se apaga el led rojo
-  delay(2000);
+   if(digitalRead(boton_1)==LOW){      //hasta que que el boton cambie a low
+    digitalWrite(led_verde,LOW);    //se apaga led verde
+    delay(1000);                                    //se da un tiempo de espera de transicion
+    digitalWrite(led_amarillo,HIGH);      //se prende el led amarillo 
+    delay(2000);                                       //se da un tiempo en el que el led amarillo estará encendido
+    digitalWrite(led_amarillo,LOW);     //se apaga el led amarillo
+    delay(2000);                                         //tiempo de espera de transicion entre led amarillo y rojo
+    digitalWrite(led_rojo,HIGH);              //se enciende el led rojo 
+    delay(4000);                                        //tiempo en el que el led rojo estara encendido
+    digitalWrite(led_rojo,LOW);             //se apaga el led rojo
+    delay(2000);
+    
+   }
+
 }
